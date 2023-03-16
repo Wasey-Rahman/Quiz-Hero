9876c448b14708c41725dda8f3623944a66cec42
Wasey Rahman <wasey123435@gmail.com>
2023-03-16 12:22:38 +0600

    adding new html file name question.html

--- index.js
+++ index.js
@@ -46,17 +46,21 @@ startQuiz.addEventListener("click", () => {
 // All quiz data fetched from json
 const loadQuiz = async () => {
   const res = await fetch("./data/quiz.json");
-  const data = await res.json;
+  const data = await res.json();
   quizData = data;
-  displayQuiz(data);
+  displayQuiz(quizData.data);
 };
 
 // Displaying quiz on quiz page
+
 const displayQuiz = (data) => {
   if (!data) {
     quizContainer.innerHTML = "";
     return;
   }
+
+  
+ 
 
   data.forEach((quiz, i) => {
     quizContainer.innerHTML += `<div class="m-3 py-3 px-4 shadow-sm rounded">
@@ -70,11 +74,12 @@ const displayQuiz = (data) => {
     ${displayQuizOptions(quiz.options, i)}
   </div>
 </div>`;
+
   });
 };
 
 // EventListener for quiz submit button
-document.querySelector("#submit").addEventlistener("click", () => {
+document.querySelector("#submit").addEventListener("click", () => {
   if (answers.length < 6) {
     return;
   }
